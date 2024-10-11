6886ea97e18ace79e15b2ec38ca3bd34117a06df
The Adventurer <leiurusrint@gmail.com>
2024-10-11 14:53:43 +0500

    [First Commit] The first encounter

---
+++ src/main/java/com/example/roofparser/RoofParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.roofparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoofParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoofParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roofparser/RoofParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roofparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoofParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
